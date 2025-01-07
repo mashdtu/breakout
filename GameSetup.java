009ea1b25ba403914dee232c232a8f796814b48d
Nikolaj <s245909@dtu.dk>
2025-01-07 15:16:47 +0100

    fix

--- GameSetup.java
+++ GameSetup.java
@@ -6,12 +6,17 @@ public class GameSetup
 
     private int xSize = 7;
     private int ySize = 4; 
+
+    //objects
     private Block[][] nrOfTargets = new Block[xSize][ySize];
+    private Platform platform;
+    private Wall[] wall = new Wall[3];
 
     public void drawStartGame()
     {
         drawBlocks();
         drawWalls();
+        drawPlatform();
     }
 
     private void drawBlocks()
@@ -28,11 +33,23 @@ public class GameSetup
         }
     }
 
-    public void drawWalls()
+    private void drawWalls()
     {
-        StdDraw.filledRectangle(90, 350, 5, 200);
-        StdDraw.filledRectangle(740, 350, 5, 200);
-        StdDraw.filledRectangle(410, 545, 325, 5);
+        wall[0] = new Wall(90, 350, 5, 200);
+        wall[1] = new Wall(740, 350, 5, 200);
+        wall[2] = new Wall(410, 545, 325, 5);
+
+        //StdDraw.filledRectangle(90, 350, 5, 200);
+        //StdDraw.filledRectangle(740, 350, 5, 200);
+        //StdDraw.filledRectangle(410, 545, 325, 5);
+    }
+
+    private void drawPlatform()
+    {
+        //x, y, w, h, speed
+        //platform = new Platform(400, 150, 50, 5, 0.02);
+
+        //platform.draw();
     }
 
    
